34c0858aba3313a3173404fe11b3e6f8701bb93b
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2023-07-01 00:08:29 +0200

    Updated build.gradle, Relicensed under LGPL-3.0

--- src/main/java/net/raphimc/mcauth/util/MicrosoftConstants.java
+++ src/main/java/net/raphimc/mcauth/util/MicrosoftConstants.java
@@ -2,10 +2,10 @@
  * This file is part of MinecraftAuth - https://github.com/RaphiMC/MinecraftAuth
  * Copyright (C) 2023 RK_01/RaphiMC and contributors
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -44,6 +44,7 @@ public class MicrosoftConstants {
 
     public static String LIVE_OAUTH_DESKTOP_URL = "https://login.live.com/oauth20_desktop.srf";
 
+    public static final String XBL_XSTS_RELYING_PARTY = "http://xboxlive.com";
     public static final String JAVA_XSTS_RELYING_PARTY = "rp://api.minecraftservices.com/";
     public static final String BEDROCK_XSTS_RELYING_PARTY = "https://multiplayer.minecraft.net/";
     public static final String BEDROCK_PLAY_FAB_XSTS_RELYING_PARTY = "https://b980a380.minecraft.playfabapi.com/";
