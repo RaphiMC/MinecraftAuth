11c692a51749d5d96517423249a1634ead8930b1
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2023-08-23 21:24:17 +0200

    Updated Mojang public key

--- src/main/java/net/raphimc/mcauth/step/bedrock/StepMCChain.java
+++ src/main/java/net/raphimc/mcauth/step/bedrock/StepMCChain.java
@@ -49,7 +49,7 @@ public class StepMCChain extends AbstractStep<StepXblXstsToken.XblXsts<?>, StepM
 
     public static final String MINECRAFT_LOGIN_URL = "https://multiplayer.minecraft.net/authentication";
 
-    private static final String MOJANG_PUBLIC_KEY_BASE64 = "MHYwEAYHKoZIzj0CAQYFK4EEACIDYgAE8ELkixyLcwlZryUQcu1TvPOmI2B7vX83ndnWRUaXm74wFfa5f/lwQNTfrLVHa2PmenpGI6JhIMUJaWZrjmMj90NoKNFSNBuKdm8rYiXsfaz3K36x/1U26HpG0ZxK/V1V";
+    private static final String MOJANG_PUBLIC_KEY_BASE64 = "MHYwEAYHKoZIzj0CAQYFK4EEACIDYgAECRXueJeTDqNRRgJi/vlRufByu/2G0i2Ebt6YMar5QX/R0DIIyrJMcUpruK4QveTfJSTp3Shlq4Gk34cD/4GUWwkv0DVuzeuB+tXija7HBxii03NHDbPAD0AKnLr2wdAp";
     private static final ECPublicKey MOJANG_PUBLIC_KEY;
     private static final int CLOCK_SKEW = 60;
 
